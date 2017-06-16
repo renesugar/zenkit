bc132ed42a8b3ebea82bfc1fe5e85dea5f03a943
Ian McCracken <ian.mccracken@gmail.com>
2017-06-16 12:08:45 -0500

    Update zenkit

--- service.go
+++ service.go
@@ -8,13 +8,14 @@ import (
 func NewService(name string) *goa.Service {
 
 	svc := goa.New(name)
+
+	svc.WithLogger(ServiceLogger())
+
 	svc.Use(middleware.RequestID())
-	svc.Use(middleware.LogRequest(false))
+	svc.Use(middleware.LogRequest(true))
 	svc.Use(MetricsMiddleware())
 	svc.Use(middleware.ErrorHandler(svc, true))
 	svc.Use(middleware.Recover())
-
-	svc.WithLogger(ServiceLogger())
 
 	return svc
 }
