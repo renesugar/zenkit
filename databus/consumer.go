8d1ba2869d73e2c60395d54e162aee9818f6b8b4
John Hamilton <johnhamilton@zenoss.com>
2017-08-02 13:44:02 -0500

    Add DatabusConsumer and tests
    savepoint

--- databus/consumer.go
+++ databus/consumer.go
@@ -1,67 +1,195 @@
 package databus
 
-//import (
-//	cluster "github.com/bsm/sarama-cluster"
-//	"context"
-//	"github.com/Shopify/sarama"
-//)
-//
-//type MessageConsumer interface {
-//	Receive(Message) Message
-//	Close() error
-//}
-//
-//func NewMessageConsumer(ctx context.Context, consumer cluster.Consumer) MessageConsumer {
-//	return &kafkaConsumer{
-//		con: consumer,
-//		messages: make(chan Message),
-//	}
-//}
-//
-//type SaramaMessage struct {
-//	Message *sarama.ConsumerMessage
-//}
-//
-//func (m *SaramaMessage) Topic() string {
-//	return m.Message.Topic
-//}
-//
-//func (m *SaramaMessage) Key() []byte {
-//	return m.Message.Key
-//}
-//
-//func (m *SaramaMessage) Value() []byte {
-//	return m.Message.Value
-//}
-//
-//type kafkaConsumer struct {
-//	con      cluster.Consumer
-//	messages chan Message
-//}
-//
-//func (p *kafkaConsumer) start(ctx context.Context) {
-//	for {
-//		select {
-//		case msg, more := <-p.con.Messages():
-//			if more {
-//				wrappedMessage := &SaramaMessage{msg}
-//				p.messages <- wrappedMessage
-//				p.con.MarkOffset(msg, "")        // mark message as processed
-//			}
-//		case <-p.con.Errors():
-//		// TODO
-//		case <-p.con.Notifications():
-//		// TODO
-//		case <-ctx.Done():
-//			return
-//		}
-//	}
-//}
-//
-//func (p *kafkaConsumer) Messages() <-chan Message {
-//	return p.messages
-//}
-//
-//func (p *kafkaConsumer) Close() error {
-//	return p.con.Close()
-//}
+import (
+	"context"
+	"errors"
+	"reflect"
+
+	"github.com/Shopify/sarama"
+	cluster "github.com/bsm/sarama-cluster"
+	"github.com/datamountaineer/schema-registry"
+)
+
+const STRUCT_KEY_TAG = "message-key"
+const STRUCT_VALUE_TAG = "message-value"
+const STRUCT_TAG_IDENTIFIER = "zenkit"
+
+var (
+	ErrInvalidMessageType = errors.New("invalid message type")
+	ErrConsumerClosed     = errors.New("consumer is closed")
+	ErrMessageNotSettable = errors.New("Message key or value is not settable")
+)
+
+type DatabusConsumer interface {
+	Consume(context.Context, interface{}) error
+	Close() error
+}
+
+func NewDatabusConsumer(brokers []string, schemaRegistry, topic, keySubject, valueSubject, groupId string) (DatabusConsumer, error) {
+	// Get our schema registry
+	schemaRegistryClient, err := schemaregistry.NewClient(schemaRegistry)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get our message factory
+	messageFactory, err := NewMessageFactory(topic, keySubject, valueSubject, schemaRegistryClient)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get our sarama cluster consumer
+	// init (custom) config, disable errors and notifications
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = false
+	config.Group.Return.Notifications = false
+
+	consumer, err := cluster.NewConsumer(brokers, groupId, []string{topic}, config)
+	if err != nil {
+		return nil, err
+	}
+	return NewSaramaClusterDatabusConsumer(consumer, messageFactory)
+}
+
+func NewSaramaClusterDatabusConsumer(consumer SaramaClusterConsumer, messageFactory MessageFactory) (DatabusConsumer, error) {
+
+	c := &saramaClusterDatabusConsumer{
+		con:            consumer,
+		messageFactory: messageFactory,
+	}
+	return c, nil
+}
+
+type SaramaClusterConsumer interface {
+	Errors() <-chan error
+	Notifications() <-chan *cluster.Notification
+	Messages() <-chan *sarama.ConsumerMessage
+	MarkOffset(*sarama.ConsumerMessage, string)
+	Close() error
+}
+
+type saramaClusterDatabusConsumer struct {
+	con            SaramaClusterConsumer
+	messageFactory MessageFactory
+}
+
+func (c *saramaClusterDatabusConsumer) Consume(ctx context.Context, v interface{}) error {
+	// Make sure what was passed in is a pointer to messageType
+	keyField, valueField, err := c.validateType(v)
+	if err != nil {
+		return err
+	}
+
+	// Get errors and notifications first
+	stop := false
+	for !stop {
+		select {
+		case <-c.con.Errors():
+		// TODO
+		case <-c.con.Notifications():
+		// TODO
+		case <-ctx.Done():
+			stop = true
+		default:
+			stop = true
+		}
+	}
+
+	select {
+	case msg, more := <-c.con.Messages():
+		if more {
+			err := c.decodeMessage(msg, v, keyField, valueField)
+			if err != nil {
+				return err
+			}
+
+			c.con.MarkOffset(msg, "") // mark message as processed
+			return nil
+		} else {
+			return ErrConsumerClosed
+		}
+	case <-ctx.Done():
+		return ErrConsumerClosed
+	}
+}
+
+func (c *saramaClusterDatabusConsumer) Close() error {
+	return c.con.Close()
+}
+
+func (c *saramaClusterDatabusConsumer) validateType(message interface{}) (int, int, error) {
+	// Make sure our message type is valid
+	//  Must be a pointer to struct with fields tagged as follows:
+	//   `zenkit:"message-key"`
+	//   `zenkit:"message-value"`
+	messageType := reflect.TypeOf(message)
+	if messageType.Kind() != reflect.Ptr {
+		return 0, 0, ErrInvalidMessageType
+	}
+	mType := messageType.Elem()
+	if mType.Kind() != reflect.Struct {
+		return 0, 0, ErrInvalidMessageType
+	}
+	keyField := -1
+	valueField := -1
+	for i := 0; i < mType.NumField(); i++ {
+		tag := mType.Field(i).Tag.Get(STRUCT_TAG_IDENTIFIER)
+		if tag == STRUCT_KEY_TAG {
+			keyField = i
+		} else if tag == STRUCT_VALUE_TAG {
+			valueField = i
+		}
+	}
+
+	if keyField < 0 || valueField < 0 {
+		return 0, 0, ErrInvalidMessageType
+	}
+
+	if !reflect.ValueOf(message).Elem().Field(keyField).CanSet() {
+		return 0, 0, ErrInvalidMessageType
+	}
+
+	if !reflect.ValueOf(message).Elem().Field(valueField).CanSet() {
+		return 0, 0, ErrInvalidMessageType
+	}
+
+	return keyField, valueField, nil
+}
+
+func (c *saramaClusterDatabusConsumer) decodeMessage(rawMsg *sarama.ConsumerMessage, v interface{}, keyField, valueField int) error {
+	messageType := reflect.TypeOf(v).Elem()
+
+	// Get a Message object we can pass to MessageFactory.Decode
+	wrappedMessage := &SaramaMessage{rawMsg}
+
+	// Decode the Key and Value
+	key := reflect.New(messageType.Field(keyField).Type).Interface()
+	value := reflect.New(messageType.Field(valueField).Type).Interface()
+	err := c.messageFactory.Decode(wrappedMessage, key, value)
+	if err != nil {
+		return err
+	}
+
+	// Populate v with the key and value
+	reflect.ValueOf(v).Elem().Field(keyField).Set(reflect.ValueOf(key).Elem())
+	reflect.ValueOf(v).Elem().Field(valueField).Set(reflect.ValueOf(value).Elem())
+
+	return nil
+
+}
+
+// Simple wrapper to make a sarama ConsumerMessage implement Message
+type SaramaMessage struct {
+	Message *sarama.ConsumerMessage
+}
+
+func (m *SaramaMessage) Topic() string {
+	return m.Message.Topic
+}
+
+func (m *SaramaMessage) Key() []byte {
+	return m.Message.Key
+}
+func (m *SaramaMessage) Value() []byte {
+	return m.Message.Value
+}
---
+++ databus/consumer_test.go
@@ -0,0 +1,447 @@
+package databus_test
+
+import (
+	"github.com/Shopify/sarama"
+	. "github.com/zenoss/zenkit/databus"
+	"github.com/zenoss/zenkit/test"
+
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/bsm/sarama-cluster"
+	"github.com/datamountaineer/schema-registry"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func newMockClusterConsumer(chanSize int) *mockClusterConsumer {
+	return &mockClusterConsumer{
+		messages:      make(chan *sarama.ConsumerMessage, chanSize),
+		errors:        make(chan error, chanSize),
+		notifications: make(chan *cluster.Notification, chanSize),
+		err:           nil,
+	}
+}
+
+type mockClusterConsumer struct {
+	messages      chan *sarama.ConsumerMessage
+	errors        chan error
+	notifications chan *cluster.Notification
+
+	err error
+}
+
+func (c *mockClusterConsumer) Messages() <-chan *sarama.ConsumerMessage {
+	return c.messages
+}
+
+func (c *mockClusterConsumer) Errors() <-chan error {
+	return c.errors
+}
+
+func (c *mockClusterConsumer) Notifications() <-chan *cluster.Notification {
+	return c.notifications
+}
+
+func (c *mockClusterConsumer) Close() error {
+	if c.err != nil {
+		return c.err
+	}
+	close(c.messages)
+	return nil
+}
+
+func (c *mockClusterConsumer) MarkOffset(msg *sarama.ConsumerMessage, metadata string) {
+	return
+}
+
+type TestMessageType struct {
+	TestKey   string `zenkit:"message-key"`
+	TestValue int    `zenkit:"message-value"`
+}
+
+type TestMessageTypeStruct struct {
+	TestKey   KeyTest `zenkit:"message-key"`
+	TestValue ValTest `zenkit:"message-value"`
+}
+
+type TestMessageTypeStructPtr struct {
+	TestKey   *KeyTest `zenkit:"message-key"`
+	TestValue *ValTest `zenkit:"message-value"`
+}
+
+var _ = Describe("Consumer", func() {
+
+	var (
+		clusterConsumer      *mockClusterConsumer
+		databusConsumer      DatabusConsumer
+		topic                string
+		schemaRegistryClient schemaregistry.Client
+		err                  error
+		messageFactory       MessageFactory
+		messagesFromKafka    []*sarama.ConsumerMessage
+		expectation          []interface{}
+		keySchema            string
+		valueSchema          string
+		schemas              = map[string]string{
+			"object-key":   `"string"`,
+			"object-value": `"int"`,
+			"key-test":     keyTestSchema,
+			"val-test":     valTestSchema,
+		}
+		ids = map[string]int{
+			"object-key":   1,
+			"object-value": 2,
+			"key-test":     3,
+			"val-test":     4,
+		}
+	)
+
+	BeforeEach(func() {
+		topic = test.RandString(8)
+		messagesFromKafka = []*sarama.ConsumerMessage{}
+		expectation = []interface{}{}
+		keySchema = "object-key"
+		valueSchema = "object-value"
+		schemaRegistryClient = GetSchemaRegistryMockClient(schemas, ids)
+		err = nil
+	})
+
+	JustBeforeEach(func() {
+		messageFactory, err = NewMessageFactory(topic, keySchema, valueSchema, schemaRegistryClient)
+		Ω(err).ShouldNot(HaveOccurred())
+
+		clusterConsumer = newMockClusterConsumer(len(messagesFromKafka))
+		done := make(chan interface{})
+		go func() {
+			for _, msg := range messagesFromKafka {
+				clusterConsumer.messages <- msg
+			}
+			close(done)
+		}()
+		Eventually(done).Should(BeClosed())
+
+		databusConsumer, err = NewSaramaClusterDatabusConsumer(clusterConsumer, messageFactory)
+	})
+
+	Context("with primitive keys and values", func() {
+		BeforeEach(func() {
+			for i := 0; i < 5; i++ {
+				key := test.RandString(8)
+				value := rand.Intn(100)
+
+				saramaMessage := &sarama.ConsumerMessage{
+					Key:   []byte(fmt.Sprintf(`"%s"`, key)),
+					Value: []byte(strconv.Itoa(value)),
+				}
+				messagesFromKafka = append(messagesFromKafka, saramaMessage)
+				expectation = append(expectation, TestMessageType{key, value})
+			}
+		})
+
+		It("should receive messages through a sarama-cluster consumer", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			var message TestMessageType
+			done := make(chan interface{})
+			go func() {
+				for i := 0; i < 5; i++ {
+					err = databusConsumer.Consume(context.Background(), &message)
+					Ω(err).ShouldNot(HaveOccurred())
+					Ω(message.TestKey).Should(Equal(expectation[i].(TestMessageType).TestKey))
+					Ω(message.TestValue).Should(Equal(expectation[i].(TestMessageType).TestValue))
+				}
+
+				close(done)
+			}()
+			Eventually(done).Should(BeClosed())
+
+			// Attempting to consume again should block
+			done = make(chan interface{})
+			var consumeErr error
+			go func() {
+				consumeErr = databusConsumer.Consume(context.Background(), &message)
+				close(done)
+			}()
+
+			Consistently(done).ShouldNot(BeClosed())
+
+			// Closing the databusConsumer will cause it to return an error
+			err = databusConsumer.Close()
+			Ω(err).ShouldNot(HaveOccurred())
+			Eventually(done).Should(BeClosed())
+			Eventually(clusterConsumer.Messages()).Should(BeClosed())
+			Ω(consumeErr).Should(Equal(ErrConsumerClosed))
+		})
+
+		It("should fail if we pass a value that isn't a pointer", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message int
+			err = databusConsumer.Consume(context.Background(), message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if we pass a pointer to something that isn't a struct", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message int
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if our struct doesn't have a 'message-key' field", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message struct {
+				TestKey   string
+				TestValue int `zenkit:"message-value"`
+			}
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if our struct doesn't have a 'message-value' field", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message struct {
+				TestKey   string `zenkit:"message-key"`
+				TestValue int
+			}
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if our key field is not settable", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message struct {
+				testKey   string `zenkit:"message-key"`
+				TestValue int    `zenkit:"message-value"`
+			}
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if our value field is not settable", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message struct {
+				TestKey   string `zenkit:"message-key"`
+				testValue int    `zenkit:"message-value"`
+			}
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should fail if our field types don't match the incoming message", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+			var message struct {
+				TestKey   int    `zenkit:"message-key"`
+				TestValue string `zenkit:"message-value"`
+			}
+			err = databusConsumer.Consume(context.Background(), &message)
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should consume all errors from the error channel", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			clusterConsumer.errors <- errors.New("A")
+			clusterConsumer.errors <- errors.New("B")
+
+			var message TestMessageType
+			done := make(chan interface{})
+			go func() {
+				defer close(done)
+				for err == nil {
+					err = databusConsumer.Consume(context.Background(), &message)
+				}
+			}()
+
+			Consistently(done).ShouldNot(BeClosed())
+			Consistently(clusterConsumer.errors).ShouldNot(Receive())
+
+			databusConsumer.Close()
+
+			Eventually(done).Should(BeClosed())
+		})
+
+		It("should consume all notifications from the notification channel", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			clusterConsumer.notifications <- &cluster.Notification{}
+			clusterConsumer.notifications <- &cluster.Notification{}
+
+			var message TestMessageType
+			done := make(chan interface{})
+			go func() {
+				defer close(done)
+				for err == nil {
+					err = databusConsumer.Consume(context.Background(), &message)
+				}
+			}()
+
+			Consistently(done).ShouldNot(BeClosed())
+			Consistently(clusterConsumer.notifications).ShouldNot(Receive())
+
+			databusConsumer.Close()
+
+			Eventually(done).Should(BeClosed())
+		})
+
+		It("should error on close if the underlying consumer errors on close", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			clusterConsumer.err = errors.New("Error!")
+			err = databusConsumer.Close()
+			Ω(err).Should(HaveOccurred())
+		})
+
+		It("should stop consuming when the context is cancelled", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			var message TestMessageType
+			done := make(chan interface{})
+			myctx, cancel := context.WithCancel(context.Background())
+			go func() {
+				defer close(done)
+				for err == nil {
+					err = databusConsumer.Consume(myctx, &message)
+				}
+			}()
+
+			Consistently(done).ShouldNot(BeClosed())
+
+			cancel()
+			Eventually(done).Should(BeClosed())
+			Ω(err).Should(Equal(ErrConsumerClosed))
+		})
+
+		It("should return immediately if the context is cancelled first", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			var message TestMessageType
+			done := make(chan interface{})
+			myctx, cancel := context.WithCancel(context.Background())
+			cancel()
+			go func() {
+				defer close(done)
+				for err == nil {
+					err = databusConsumer.Consume(myctx, &message)
+				}
+			}()
+			Eventually(done).Should(BeClosed())
+			Ω(err).Should(Equal(ErrConsumerClosed))
+		})
+	})
+
+	Context("with struct keys and values", func() {
+		BeforeEach(func() {
+			keySchema = "key-test"
+			valueSchema = "val-test"
+			for i := 0; i < 5; i++ {
+				key := KeyTest{test.RandString(8), rand.Intn(100)}
+				value := ValTest{test.RandString(8)}
+
+				keyjson, err := json.Marshal(key)
+				Ω(err).ShouldNot(HaveOccurred())
+				valjson, err := json.Marshal(value)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				saramaMessage := &sarama.ConsumerMessage{
+					Key:   keyjson,
+					Value: valjson,
+				}
+				messagesFromKafka = append(messagesFromKafka, saramaMessage)
+				expectation = append(expectation, TestMessageTypeStruct{key, value})
+			}
+		})
+
+		It("should receive messages through a sarama-cluster consumer", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			message := TestMessageTypeStruct{}
+			done := make(chan interface{})
+			go func() {
+				for i := 0; i < 5; i++ {
+					err = databusConsumer.Consume(context.Background(), &message)
+					Ω(err).ShouldNot(HaveOccurred())
+					Ω(message.TestKey).Should(Equal(expectation[i].(TestMessageTypeStruct).TestKey))
+					Ω(message.TestValue).Should(Equal(expectation[i].(TestMessageTypeStruct).TestValue))
+				}
+
+				close(done)
+			}()
+			Eventually(done).Should(BeClosed())
+		})
+	})
+
+	Context("with struct pointer keys and values", func() {
+		BeforeEach(func() {
+			keySchema = "key-test"
+			valueSchema = "val-test"
+			for i := 0; i < 5; i++ {
+				key := &KeyTest{test.RandString(8), rand.Intn(100)}
+				value := &ValTest{test.RandString(8)}
+
+				keyjson, err := json.Marshal(key)
+				Ω(err).ShouldNot(HaveOccurred())
+				valjson, err := json.Marshal(value)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				saramaMessage := &sarama.ConsumerMessage{
+					Key:   keyjson,
+					Value: valjson,
+				}
+				messagesFromKafka = append(messagesFromKafka, saramaMessage)
+				expectation = append(expectation, TestMessageTypeStructPtr{key, value})
+			}
+		})
+
+		It("should receive messages through a sarama-cluster consumer", func() {
+			Ω(err).ShouldNot(HaveOccurred())
+
+			message := TestMessageTypeStructPtr{}
+			done := make(chan interface{})
+			go func() {
+				for i := 0; i < 5; i++ {
+					err = databusConsumer.Consume(context.Background(), &message)
+					Ω(err).ShouldNot(HaveOccurred())
+					Ω(message.TestKey).Should(Equal(expectation[i].(TestMessageTypeStructPtr).TestKey))
+					Ω(message.TestValue).Should(Equal(expectation[i].(TestMessageTypeStructPtr).TestValue))
+				}
+
+				close(done)
+			}()
+			Eventually(done).Should(BeClosed())
+		})
+	})
+})
+
+var _ = Describe("SaramaMessage", func() {
+	var (
+		topic         string
+		key           []byte
+		value         []byte
+		saramaMessage *sarama.ConsumerMessage
+	)
+
+	BeforeEach(func() {
+		topic = test.RandString(8)
+		key = []byte(test.RandString(8))
+		value = []byte(test.RandString(8))
+
+		saramaMessage = &sarama.ConsumerMessage{
+			Topic: topic,
+			Key:   key,
+			Value: value,
+		}
+	})
+
+	It("should have the same topic, key, and value as the sarama message", func() {
+		msg := SaramaMessage{saramaMessage}
+		Ω(msg.Key()).Should(Equal(key))
+		Ω(msg.Value()).Should(Equal(value))
+		Ω(msg.Topic()).Should(Equal(topic))
+	})
+})
