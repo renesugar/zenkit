accfcd7ed5719d1b19a611e60882b132e1fdec3b
Ian McCracken <ian.mccracken@gmail.com>
2017-08-24 09:23:12 -0500

    Some comments

--- test/websocket.go
+++ test/websocket.go
@@ -9,12 +9,16 @@ import (
 	"github.com/onsi/gomega/ghttp"
 )
 
+// WebSocketHandler is a generic WebSocket handler that a user would provide in
+// a test
 type WebSocketHandler func(*websocket.Conn)
 
+// RouteToWSHandler is called on a ghttp.Server to route to the websocket handler.
 func RouteToWSHandler(server *ghttp.Server, method, path string, handler WebSocketHandler) {
 	server.RouteToHandler(method, path, wshandler(handler))
 }
 
+// WebSocketURL ensures that the URL passed uses the websocket scheme.
 func WebSocketURL(server *ghttp.Server) *url.URL {
 	u, _ := url.Parse(server.URL())
 	u.Scheme = "ws"
@@ -26,6 +30,8 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+// wshandler is a trivial wrapper around a WebSocketHandler that turns it into
+// an ordinary http.HandlerFunc.
 func wshandler(h WebSocketHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		conn, err := upgrader.Upgrade(w, req, nil)
