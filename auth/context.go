74e850a51875c5aab4af1f68a3980de96ccac7ac
Summer Mousa <smousa@zenoss.com>
2018-01-04 08:04:38 -0600

    do not validate the auth token

--- auth/context.go
+++ auth/context.go
@@ -2,15 +2,16 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	jwtgo "github.com/dgrijalva/jwt-go"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware/security/jwt"
 	"github.com/pkg/errors"
 	"github.com/zenoss/zenkit/claims"
-	"github.com/zenoss/zenkit/logging"
 )
 
 type key int
@@ -30,13 +31,36 @@ func ContextIdentity(ctx context.Context) Identity {
 	return nil
 }
 
-func JWTMiddleware(logger logging.ErrorLogger, filename string, validator goa.Middleware, security *goa.JWTSecurity) (goa.Middleware, error) {
-	key, err := ReadKeyFromFS(logger, filename)
-	if err != nil {
-		return nil, errors.Wrap(err, "couldn't read key from filesystem")
+func JWTMiddleware(h goa.Handler) goa.Handler {
+	return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+		val := req.Header.Get("Authorization")
+		if val == "" {
+			return jwt.ErrJWTError("missing header \"Authorization\"")
+		}
+
+		parts := strings.Split(val, ".")
+		if len(parts) != 3 {
+			return jwt.ErrJWTError("JWT validation failed")
+		}
+
+		// parse Claims
+		var mapClaims jwtgo.MapClaims
+		claimBytes, _ := jwtgo.DecodeSegment(parts[1])
+		err := json.Unmarshal(claimBytes, &mapClaims)
+		if err != nil {
+			return jwt.ErrJWTError("JWT validation failed")
+		}
+
+		stdClaims := claims.StandardClaimsMap(mapClaims)
+		ident := &tokenIdentity{stdClaims}
+		ctx = WithIdentity(ctx, ident)
+		ctx = goa.WithLogContext(ctx, "user_id", ident.ID())
+		if len(ident.Tenant()) == 0 {
+			return errors.New("unable to retrieve tenant from token")
+		}
+
+		return h(ctx, rw, req)
 	}
-	resolver := jwt.NewSimpleResolver([]jwt.Key{key})
-	return jwt.New(resolver, validator, security), nil
 }
 
 func DevModeMiddleware(h goa.Handler) goa.Handler {
@@ -50,25 +74,5 @@ func DevModeMiddleware(h goa.Handler) goa.Handler {
 			req.Header.Set("Authorization", devJWT)
 		}
 		return h(ctx, rw, req)
-	}
-}
-
-func JWTValidatorFunc(m JWTValidator) goa.Middleware {
-	return func(h goa.Handler) goa.Handler {
-		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
-			if err := m(ctx); err != nil {
-				return errors.WithStack(err)
-			}
-			token := jwt.ContextJWT(ctx)
-			stdClaims := claims.StandardClaimsMap(token.Claims.(jwtgo.MapClaims))
-			ident := &tokenIdentity{stdClaims}
-			ctx = WithIdentity(ctx, ident)
-			ctx = goa.WithLogContext(ctx, "user_id", ident.ID())
-			if len(ident.Tenant()) == 0 {
-				message := "Unable to retrieve tenant from token"
-				return errors.WithStack(errors.New(message))
-			}
-			return h(ctx, rw, req)
-		}
 	}
 }
